5adab09d7edcba6829f0d36724c53516b0e0d37e
Ardii17 <139063897+Ardii17@users.noreply.github.com>
2025-01-13 13:10:47 +0700

    Update classControllers.js

--- src/controllers/classControllers.js
+++ src/controllers/classControllers.js
@@ -6,11 +6,11 @@ class ClassController {
     try {
       // Destructure with default values
       const {
-        nama_class = "Ardi",
-        deskripsi = "Ardi",
-        hari = "Senin",
-        teacher = "Ardi",
-        time = "10:00:00",
+        nama_class = null,
+        deskripsi = null,
+        hari = null,
+        teacher = null,
+        time = null,
       } = req.body;
 
       const [result] = await pool.execute(
