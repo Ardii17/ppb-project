4508b66794c51156440df2844695d0a8b69c3b24
Ardii17 <139063897+Ardii17@users.noreply.github.com>
2025-02-06 07:56:09 +0700

    Update assignRoutes.js

--- src/routes/assignRoutes.js
+++ src/routes/assignRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const AssignController = require("../controllers/assignControllers");
 const router = express.Router();
 
-router.get("/", AssignController.getAssignmentsByClass);
-router.post("/", AssignController.createAssignments);
+router.get("/assignments", AssignController.readAssignments);
+router.post("/assignments", AssignController.createAssignments);
+router.put("/assignments/:id", AssignController.updateAssignments);
+router.delete("/assignments/:id", AssignController.deleteAssignments);
 
 module.exports = router;
